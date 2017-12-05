45856b11610c324dd4a88bed55616d4e77fae8a7
mkan0141 <kanzume0141@gmail.com>
2017-12-05 19:14:31 +0900

    hoge

--- src/main.cpp
+++ src/main.cpp
@@ -20,22 +20,45 @@ int main() {
 
 	game.printBoard();
 	while (!game.isEnd()) {
+
 		game.printBoard();
 		game.printPos();
+
 		if(!te){
 			x = user.input(game);
 			te = !te;
 		}else{
-			std::vector<BitBoard> vec = game.getPosVec();
-			
-			for(auto v : vec){
-				
+			// x = user.input(game);
+			// te = !te;
 
+			
+			std::vector<BitBoard> vec = game.getPosVec();
+			int value = -10000000;
+			BitBoard b, w;
+			
+			b = game.getBlack();
+			w = game.getWhite();
+
+			for(auto v : vec){
+				std::cout << v << std::endl;
+				game.changeColor(b, w);
+				int new_value = ai.NegaMax(game, 2);
+
+				// std::cout << "new value: " << new_value << " " << value << std::endl;
+				if(value < new_value){
+					// std::cout << "new!!: " << v << std::endl; 
+					value = new_value;
+					x = v;
+				} 
+				game.changeColor(b, w);
 			}
+			game.AISetPosPrint(x);
 			te = !te;
 		}
 
+		// std::cout << "set: " <<  x << std::endl;
 		game.putPos(x);
+		game.nextTurn();
 	}
 	
 }
