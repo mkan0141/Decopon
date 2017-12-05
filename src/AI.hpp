9759d6e028f8915db4936f22b2bf9365d2a9f6c2
mkan0141 <kanzume0141@gmail.com>
2017-12-05 19:14:10 +0900

    やっとこさ正常に動いた

--- src/AI.hpp
+++ src/AI.hpp
@@ -3,18 +3,37 @@
 
 class AI{
 public:
-    BitBoard NegaMax(Board board, int depth){
+    int NegaMax(Board board, int depth){
         if(depth == 0 || board.isEnd() == true){
+             // std::cout << "this value is " << eval(board) << std::endl;
+            // board.printBoard();
             return eval(board);
         }
 
-        BitBoard best = 0;
+        int best = 0;
         std::vector<BitBoard> vec = board.getPosVec();
+        BitBoard b, w;
         
+        /*  DEBUG
+        std::cout << "depth: " << depth << std::endl;
+        for(int i = 0; i < vec.size(); i++)std::cout << i << ": " << vec[i] << " ";
+        std::cout << std::endl;
+        */
+
+        // 移動前の盤面を保存
+        b = board.getBlack();
+        w = board.getWhite();
+
         for(int i = 0; i < vec.size(); i++){
-            Board nextBoard = board;
-            nextBoard.putPos(vec[i]);
-            BitBoard value = max(value, -NegaMax(board, depth - 1 ));
+
+            // error: 参照渡しっぽい ?? 
+            board.putPos(vec[i]);
+            board.nextTurn();
+            //board.printBoard();
+            best = max(best, -NegaMax(board, depth - 1 ));
+            board.changeColor(b, w);
+
+            board.undoTurn();
         }
 
         return best;
@@ -32,16 +51,17 @@ private:
         30, -12,  0, -1, -1,  0, -12, 30
     };
 
-    BitBoard eval(Board board){
+    int eval(Board board){
         int value = 0;
-        BitBoard pos = 1;
+        BitBoard pos = (BitBoard) 1 << 63;
         BitBoard white = board.getWhite();
         BitBoard black = board.getBlack();
-        for(int i = 63; i >= 0; i--){
+        for(int i = 0; i < 63; i++){
             if((pos & white) != 0 || (pos & black) != 0){
+                // std::cout << "add weight ... " << weight[i] << " "<< std::endl;
                 value += weight[i];
             }
-            pos = (pos << 1);
+            pos >>= 1;
         }
     }
 
