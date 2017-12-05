bf4e76df38831b7e0e96d8eb195066c3b47066c3
mkan0141 <kanzume0141@gmail.com>
2017-12-05 22:00:35 +0900

    おそらく完成

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "Board.hpp"
 #include "User.hpp"
 #include "AI.hpp"
-
+#include "RandomAI.hpp"
 
 int main() {
 	int a;
@@ -12,12 +12,13 @@ int main() {
 	BitBoard x;
 	Board game;
 	User user;
+	RandomAI rm;
 	AI ai;
 
-
-	std::cout << "player (先手 0 後手 1) >> " ;
-	std::cin >> te;
-
+	//std::cout << "player (先手 0 後手 1) >> " ;
+	//std::cin >> te;
+	
+	te = false;
 	game.printBoard();
 	while (!game.isEnd()) {
 
@@ -25,14 +26,48 @@ int main() {
 		game.printPos();
 
 		if(!te){
-			x = user.input(game);
+			
+			std::vector<BitBoard> vec = game.getPosVec();
+
+			if(vec.size() == 0){
+				std::cout << "置ける場所がありません。 このターンをパスします" << std::endl;
+				if(game.passCheck() == true){
+					std::cout << "両者とも打つ場所がありません。 ゲームを終了します" << std::endl;
+					break;
+				}
+				te = !te;
+				game.nextTurn();
+				game.pass();
+				continue;
+			}
+			
+			x = rm.RandomPos(vec);
+			std::cout << "Random_AI set ";
+			game.SetPosPrint(x);
+			
+			
+			// x = user.input(game);
+			
 			te = !te;
 		}else{
+			
 			// x = user.input(game);
 			// te = !te;
 
-			
 			std::vector<BitBoard> vec = game.getPosVec();
+
+			if(vec.size() == 0){
+				std::cout << "置ける場所がありません。 このターンをパスします" << std::endl;
+				if(game.passCheck() == true){
+					std::cout << "両者とも打つ場所がありません。 ゲームを終了します" << std::endl;
+					break;
+				}
+				te = !te;
+				game.nextTurn();
+				game.pass();
+				continue;
+			}
+			
 			int value = -10000000;
 			BitBoard b, w;
 			
@@ -40,25 +75,38 @@ int main() {
 			w = game.getWhite();
 
 			for(auto v : vec){
-				std::cout << v << std::endl;
 				game.changeColor(b, w);
 				int new_value = ai.NegaMax(game, 2);
 
-				// std::cout << "new value: " << new_value << " " << value << std::endl;
 				if(value < new_value){
-					// std::cout << "new!!: " << v << std::endl; 
 					value = new_value;
 					x = v;
 				} 
 				game.changeColor(b, w);
 			}
-			game.AISetPosPrint(x);
+			std::cout << "NegaMax_AI set ";
+			game.SetPosPrint(x);
 			te = !te;
 		}
 
 		// std::cout << "set: " <<  x << std::endl;
+		game.addStone();
 		game.putPos(x);
 		game.nextTurn();
 	}
-	
-}
+	std:: cout<< std::endl << std:: endl;
+	std::cout << "----------------- Game Set !! -----------------" << std::endl;
+	game.printBoard();
+
+	int black = game.countBlack();
+	int white = game.countWhite();
+
+	std::cout << "黒: " << black << "    " << "白: " << white << std::endl;
+	if(black > white){
+		std::cout << "黒の勝利" << std::endl;
+	}else if(black < white){
+		std::cout << "白の勝利" << std::endl;
+	}else{
+		std::cout << "引き分け" << std::endl;
+	}
+ }
