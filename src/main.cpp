b9db488ed53c8433044a23b5538051e11f172874
mkan0141 <kanzume0141@gmail.com>
2017-12-07 13:09:04 +0900

    といあえず動くようになったよ

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,26 @@
 #include "AI.hpp"
 #include "RandomAI.hpp"
 
-int main() {
-	int a;
-	char c;
-	bool te;
-	BitBoard x;
-	Board game;
-	User user;
-	RandomAI rm;
-	AI ai;
+int a;
+char c;
+bool te;
+BitBoard x;
+Board game;
+User user;
+RandomAI rm;
+AI ai;
 
-	//std::cout << "player (先手 0 後手 1) >> " ;
-	//std::cin >> te;
-	
-	te = false;
+void gameWhite(){
+	te = !te;
 	game.printBoard();
+	std::cout << "Debug: " << te << " " << game.getColor() << std::endl;
 	while (!game.isEnd()) {
 
 		game.printBoard();
 		game.printPos();
 
-		if(!te){
-			
+		if(te){
+
 			std::vector<BitBoard> vec = game.getPosVec();
 
 			if(vec.size() == 0){
@@ -40,19 +38,13 @@ int main() {
 				game.pass();
 				continue;
 			}
-			
-			x = rm.RandomPos(vec);
-			std::cout << "Random_AI set ";
+
 			game.SetPosPrint(x);
 			
-			
-			// x = user.input(game);
+			x = user.input(game);
 			
 			te = !te;
 		}else{
-			
-			// x = user.input(game);
-			// te = !te;
 
 			std::vector<BitBoard> vec = game.getPosVec();
 
@@ -76,20 +68,25 @@ int main() {
 
 			for(auto v : vec){
 				game.changeColor(b, w);
-				int new_value = ai.NegaMax(game, 2);
+				int new_value;
 
+				game.putPos(v);
+                game.nextTurn();
+				if(game.getStoneNum() >= 54) new_value = ai.dfs(game, Black);
+				else new_value = ai.MinMax(game, 5, Black);
+				game.undoTurn();
+				std::cout << "評価値: " << new_value << std::endl;
 				if(value < new_value){
 					value = new_value;
 					x = v;
 				} 
 				game.changeColor(b, w);
 			}
-			std::cout << "NegaMax_AI set ";
+			std::cout << "MinMax_AI set ";
 			game.SetPosPrint(x);
 			te = !te;
 		}
 
-		// std::cout << "set: " <<  x << std::endl;
 		game.addStone();
 		game.putPos(x);
 		game.nextTurn();
@@ -109,4 +106,110 @@ int main() {
 	}else{
 		std::cout << "引き分け" << std::endl;
 	}
+}
+
+void gameBlack(){
+	game.printBoard();
+	std::cout << "Debug: " << te << " " << game.getColor() << std::endl;
+	while (!game.isEnd()) {
+
+		game.printBoard();
+		game.printPos();
+
+		if(!te){
+
+			std::vector<BitBoard> vec = game.getPosVec();
+
+			if(vec.size() == 0){
+				std::cout << "置ける場所がありません。 このターンをパスします" << std::endl;
+				if(game.passCheck() == true){
+					std::cout << "両者とも打つ場所がありません。 ゲームを終了します" << std::endl;
+					break;
+				}
+				te = !te;
+				game.nextTurn();
+				game.pass();
+				continue;
+			}
+
+			game.SetPosPrint(x);
+			
+			x = user.input(game);
+			
+			te = !te;
+		}else{
+
+			std::vector<BitBoard> vec = game.getPosVec();
+
+			if(vec.size() == 0){
+				std::cout << "置ける場所がありません。 このターンをパスします" << std::endl;
+				if(game.passCheck() == true){
+					std::cout << "両者とも打つ場所がありません。 ゲームを終了します" << std::endl;
+					break;
+				}
+				te = !te;
+				game.nextTurn();
+				game.pass();
+				continue;
+			}
+			
+			int value = -10000000;
+			BitBoard b, w;
+			
+			b = game.getBlack();
+			w = game.getWhite();
+
+			for(auto v : vec){
+				game.changeColor(b, w);
+				int new_value;
+
+				game.putPos(v);
+                game.nextTurn();
+				if(game.getStoneNum() >= 54) new_value = ai.dfs(game, White);
+				else new_value = ai.MinMax(game, 5, White);
+				game.undoTurn();
+				std::cout << "評価値: " << new_value << std::endl;
+				if(value < new_value){
+					value = new_value;
+					x = v;
+				} 
+				game.changeColor(b, w);
+			}
+			std::cout << "MinMax_AI set ";
+			game.SetPosPrint(x);
+			te = !te;
+		}
+
+		game.addStone();
+		game.putPos(x);
+		game.nextTurn();
+	}
+	std:: cout<< std::endl << std:: endl;
+	std::cout << "----------------- Game Set !! -----------------" << std::endl;
+	game.printBoard();
+
+	int black = game.countBlack();
+	int white = game.countWhite();
+
+	std::cout << "黒: " << black << "    " << "白: " << white << std::endl;
+	if(black > white){
+		std::cout << "黒の勝利" << std::endl;
+	}else if(black < white){
+		std::cout << "白の勝利" << std::endl;
+	}else{
+		std::cout << "引き分け" << std::endl;
+	}
+}
+
+int main() {
+
+	std::cout << "player (先手 0 後手 1) >> " ;
+	std::cin >> te;
+
+	if(te == Black){
+		gameBlack();
+	}else if(te == White){
+		gameWhite();
+	}
+	
  }
